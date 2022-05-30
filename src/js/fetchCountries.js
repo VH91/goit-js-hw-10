9d898b152f3b4bd88e7da774ab7c4b826651304f
VH91 <hritsik.v.1991@gmail.com>
2022-05-30 16:57:24 +0300

    fixed

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,8 +3,6 @@ function fetchCountries(name) {
     return fetch(`${url}/${name}?fields=name,capital,population,flags,languages`).then(res => {
         if (res.ok) return res.json();
         else throw new Error("Status code error :" + res.status)
-    }).then(arr => {
-        return arr;
-    });
+    })
 }
 export default fetchCountries;
